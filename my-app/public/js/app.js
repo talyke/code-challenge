78204213a8b406ba8ff8b7936e0f89890c344af1
Tara <32590508+talyke@users.noreply.github.com>
2021-11-01 11:58:45 -0600

    setup
    react, vue, laravel

---
+++ my-app/public/js/app.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+alert('hello world');
+console.log('hello world');
+/******/ })()
+;
